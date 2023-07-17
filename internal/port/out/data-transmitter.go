12d497de0242d6a4095bc3e8907795734ea596c8
lsjtop10 <lsjtop10@gmail.com>
2023-07-17 14:00:53 +0900

    Fix TransmitDataToRemote(file entity.FileManager) to return error

--- internal/port/out/data-transmitter.go
+++ internal/port/out/data-transmitter.go
@@ -4,5 +4,5 @@ import "github.com/Goboolean/manager-cli/internal/domain/entity"
 
 type DataTransmitterPort interface {
 	//This method transmit data to remote storage
-	TransmitDataToRemote(file entity.FileManager)
+	TransmitDataToRemote(file entity.FileManager) error
 }
