64f8968c4aecd1b394cc050ada0934617a235193
lsjtop10 <lsjtop10@gmail.com>
2023-07-17 13:39:19 +0900

    Change parmeter name(identifier -> id)

--- internal/port/out/persistences.go
+++ internal/port/out/persistences.go
@@ -7,26 +7,28 @@ import (
 )
 
 type MetadataRepositoryPort interface {
-	// This method gets unique identifier of a product which can be hash, UUID and so on...
-	GetProductIdentifier(code string) (string, error)
+	// This method gets unique id of a product which can be hash, UUID and so on...
+	GetProductId(code string) (string, error)
 	// This method gets full metadata of a product
-	GetProductMeta(identifier string) (entity.ProductMeta, error)
+	GetProductMeta(id string) (entity.ProductMeta, error)
 	// This method stores metadata to metadata repository which can be mysql, radius so on...
 	StoreProductMeta(meta entity.ProductMeta) error
 }
 
 type StatusPort interface {
 	// This method returns status of a product
-	GetStatus(identifier string) entity.ProductStatus
+	GetStatus(id string) entity.ProductStatus
 	// This method changes status of a product by "status" val
-	ChangeStatus(productIdentifier string, status entity.ProductStatus) error
+	SetStatus(id string, status entity.ProductStatus) error
 }
 
 type TradeRepositoryPort interface {
 	// This method dumps trade data from trade data repository
-	DumpTradeRepo() entity.FileManager
+	DumpTradeRepo() (entity.FileManager, error)
 	// This method dumps trade data created before a specific date
-	DumpTradeRepoBefore(date time.Time) entity.FileManager
+	DumpTradeRepoBefore(date time.Time) (entity.FileManager, error)
 	// This method dumps trade data of specific product
-	DumpProduct(identifier string) entity.FileManager
+	DumpProduct(id string) (entity.FileManager, error)
+	// This method dumps trade data of specific product created before time
+	DumpProductBefore(id string, time time.Time) (entity.FileManager, error)
 }
