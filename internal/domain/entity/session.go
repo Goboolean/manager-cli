90484c38e1b5094980a2c7e6b599505b14c94c5d
lsjtop10 <lsjtop10@gmail.com>
2023-07-29 11:36:51 +0900

    Add new Session entity to mamage session of each transection abstractly

---
+++ internal/domain/entity/session.go
@@ -0,0 +1,35 @@
+package entity
+
+import (
+	"context"
+	"math/rand"
+	"time"
+)
+
+type Session struct {
+	// Unique identifier of each session
+	id  int
+	ctx context.Context
+
+	// conteains extra value of a session
+	// If loose typing becomes a problem,
+	// consider creating different structures for each usecase.
+	Info map[string]interface{}
+}
+
+func New(ctx context.Context) *Session {
+	rand.Seed(time.Now().Unix())
+
+	return &Session{
+		id:  rand.Int(),
+		ctx: ctx,
+	}
+}
+
+func (s *Session) GetId() int {
+	return s.id
+}
+
+func (s *Session) GetContext() context.Context {
+	return s.ctx
+}
