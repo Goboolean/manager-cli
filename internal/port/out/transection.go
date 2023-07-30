8f7f60350dc79f2f0dac6ad4e3bf8b3d0375870c
lsjtop10 <lsjtop10@gmail.com>
2023-07-30 20:33:34 +0900

    Fix CreateNewSession not to return error

--- internal/port/out/transection.go
+++ internal/port/out/transection.go
@@ -7,7 +7,7 @@ import (
 )
 
 type TransactorPort interface {
-	CreateNewSession(ctx context.Context) (*entity.Session, error)
+	CreateNewSession(ctx context.Context) *entity.Session
 	Begin(*entity.Session) error
 	Commit(*entity.Session) error
 	Rollback(*entity.Session) error
