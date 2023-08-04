c2448920c409b2810c9af90ed3db1f0bf0486148
lsjtop10 <lsjtop10@gmail.com>
2023-08-04 10:49:30 +0900

    Fix TransmitDataToRemote to accept list of file

--- internal/port/out/data-transmitter.go
+++ internal/port/out/data-transmitter.go
@@ -4,5 +4,5 @@ import "github.com/Goboolean/manager-cli/internal/domain/entity"
 
 type DataTransmitterPort interface {
 	//This method transmit data to remote storage
-	TransmitDataToRemote(file entity.FileManager) error
+	TransmitDataToRemote(file []entity.FileManager) error
 }
