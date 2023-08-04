46f747be57809d4422d65fdc5d6d9f0171340517
lsjtop10 <lsjtop10@gmail.com>
2023-08-04 10:49:44 +0900

    Add comment

--- internal/port/out/file.go
+++ internal/port/out/file.go
@@ -2,6 +2,7 @@ package out
 
 import "github.com/Goboolean/manager-cli/internal/domain/entity"
 
+// Defines Port for file operation ex: create, remove, copy, move
 type FilePort interface {
 	RemoveFile(target entity.FileManager) error
 }
