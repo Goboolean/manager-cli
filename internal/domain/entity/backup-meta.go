c52aad195f954f77c6f8e8e1a0eaab436fbe12b4
lsjtop10 <lsjtop10@gmail.com>
2023-08-18 14:20:46 +0900

    Add backup metadata entity

--- internal/domain/entity/backup-meta.go
+++ internal/domain/entity/backup-meta.go
@@ -1,13 +1,20 @@
 package entity
 
+const (
+	FullBackup = "full"
+	DiffBackup = "diff"
+)
+
 type BackupMeta struct {
-	BackupType   string   `json:"backupType"` // Ex:)full,diff
-	BackupDbList []string `json:"backupDb"`   // Database list
-	Timestamp    int      `json:"timestamp"`  // Unix timestamp of backup time
-	Date         string   `json:"date"`       //Human readable datetime of backup time
-	HashVer      string   `json:"hashVer"`    // Version of hash algorithm
-	FileInfoList []struct {
-		Name string `json:"fileName"`
-		Hash string `json:"fileHash"`
-	} `json:"fileInfo"`
+	BackupType   string           `json:"backupType"` // Ex:)full,diff
+	BackupDbList []string         `json:"backupDb"`   // Database list
+	Timestamp    int64            `json:"timestamp"`  // Unix timestamp of backup time
+	Date         string           `json:"date"`       //Human readable datetime of backup time
+	HashVer      string           `json:"hashVer"`    // Version of hash algorithm
+	FileInfoList []BackupFileInfo `json:"fileInfo"`
+}
+
+type BackupFileInfo struct {
+	Name string `json:"fileName"`
+	Hash string `json:"fileHash"`
 }
