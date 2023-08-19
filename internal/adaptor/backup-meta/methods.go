24b6eb05858476b6373c7543201b3044049122e3
lsjtop10 <lsjtop10@gmail.com>
2023-08-19 10:10:22 +0900

    Fix StoreBackupMeta and ImportBackupMetaFrom file to receive file entity

--- internal/adaptor/backup-meta/methods.go
+++ internal/adaptor/backup-meta/methods.go
@@ -7,20 +7,20 @@ import (
 )
 
 // This method stores backup metadata to metadata repository which can be mysql, radius so on...
-func (a *BackupMetaAdaptor) StoreBackupMeta(meta entity.BackupMeta, outDir string) error {
+func (a *BackupMetaAdaptor) StoreBackupMeta(meta entity.BackupMeta, target entity.File) error {
 
 	metaJson, err := json.Marshal(meta)
 	if err != nil {
 		return err
 	}
 
-	return a.file.StoreJsonToFile(metaJson, outDir)
+	return a.file.StoreJsonToFile(metaJson, target.FullPath())
 }
 
 // This method gets backup metadata from metadata repository which can be mysql, radius so on...
-func (a *BackupMetaAdaptor) ImportBackupMetaFromFile(file entity.File) (entity.BackupMeta, error) {
+func (a *BackupMetaAdaptor) ImportBackupMetaFromFile(target entity.File) (entity.BackupMeta, error) {
 	var res entity.BackupMeta
 
-	err := a.file.DecodeJsonFromFile(file.FullPath(), &res)
+	err := a.file.DecodeJsonFromFile(target.FullPath(), &res)
 	return res, err
 }
