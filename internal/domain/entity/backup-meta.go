f20c6a4d9a8d12625233ad6856dd849d76c31026
lsjtop10 <lsjtop10@gmail.com>
2023-08-23 10:48:53 +0900

    Rename FileInfo to FileNmaeWithHash to clarify contained values

--- internal/domain/entity/backup-meta.go
+++ internal/domain/entity/backup-meta.go
@@ -6,15 +6,15 @@ const (
 )
 
 type BackupMeta struct {
-	BackupType   string           `json:"backupType"` // Ex:)full,diff
-	BackupDbList []string         `json:"backupDb"`   // Database list
-	Timestamp    int64            `json:"timestamp"`  // Unix timestamp of backup time
-	Date         string           `json:"date"`       //Human readable datetime of backup time
-	HashVer      string           `json:"hashVer"`    // Version of hash algorithm
-	FileInfoList []BackupFileInfo `json:"fileInfo"`
+	BackupType   string             `json:"backupType"` // Ex:)full,diff
+	BackupDbList []string           `json:"backupDb"`   // Database list
+	Timestamp    int64              `json:"timestamp"`  // Unix timestamp of backup time
+	Date         string             `json:"date"`       //Human readable datetime of backup time
+	HashVer      string             `json:"hashVer"`    // Version of hash algorithm
+	FileList     []FileNameWithHash `json:"fileList"`
 }
 
-type BackupFileInfo struct {
+type FileNameWithHash struct {
 	Name string `json:"fileName"`
 	Hash string `json:"fileHash"`
 }
